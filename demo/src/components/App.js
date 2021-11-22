2da132de121d127925269aaab4a4801f7fff1a95
MichaelJosephMiller <michael.joseph.miller@gmail.com>
2021-11-21 20:33:39 -0500

    convert to one repo demo and lib together

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -12,40 +12,34 @@ import * as classes from './App.css';
 
 function App() {
   return (
-    <div className={classes.app}>
-      <div className={classes.grid}>
-        <header className={classes.header}>
-          <img
-            className={`${classes.headerImgLeft} ${classes.headerImg}`}
-            src={logo128}
-            alt=''
-          />
-          <h2 className={classes.headerTitle}>loader-animations-react</h2>
-          <img
-            className={`${classes.headerImgRight} ${classes.headerImg}`}
-            src={logo128}
-            alt=''
-          />
-        </header>
-        <main className={classes.main}>
-          <Dots />
-          <ProgressBar />
-          <Waves />
-          <Spinner />
-          <MinimalSpinner />
-          <TrinitySpinner />
-        </main>
-        <footer className={classes.footer}>
-          Icons made by
-          <a href='https://www.freepik.com' title='Freepik'>
-            Freepik
-          </a>{' '}
-          from
-          <a href='https://www.flaticon.com/' title='Flaticon'>
-            www.flaticon.com
-          </a>
-        </footer>
-      </div>
+    <div className={classes.grid}>
+      <header className={classes.header}>
+        <img className={classes.headerImg} src={logo128} alt='' />
+        <h2 className={classes.headerTitle}>loader-animations-react</h2>
+        <img
+          className={`${classes.headerImgRight} ${classes.headerImg}`}
+          src={logo128}
+          alt=''
+        />
+      </header>
+      <main className={classes.main}>
+        <Dots />
+        <ProgressBar />
+        <Waves />
+        <Spinner />
+        <MinimalSpinner />
+        <TrinitySpinner />
+      </main>
+      <footer className={classes.footer}>
+        Icons made by
+        <a href='https://www.freepik.com' title='Freepik'>
+          Freepik
+        </a>{' '}
+        from
+        <a href='https://www.flaticon.com/' title='Flaticon'>
+          www.flaticon.com
+        </a>
+      </footer>
     </div>
   );
 }
